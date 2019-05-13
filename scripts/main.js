a5f58a0da85d8fda600b20f647ca98115327fe04
Javier <parringuer@gmail.com>
2019-05-13 20:00:53 +0200

    Portadas de películas mostradas con column

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,21 @@
-import films from 'films.js';
+import films from './films.js';
 
-poster_path = 'http://image.tmdb.org/t/p/w185/';
+let poster_path = 'http://image.tmdb.org/t/p/w185/';
+window.addEventListener('load', fillFilms);
+
+function fillFilms() {
+    let showFilm = document.getElementById("showFilms");
+    showFilm.innerHTML = "";
+    let numberFilms = 8, film = {}, filmString;
+    for (let i = 0; i < numberFilms; i++) {
+        film = films[i];
+        filmString = "";
+
+        filmString += `<div class="film"><div><img src=${poster_path}${film.poster_path} alt="poster film"></div>`;
+        filmString += `<div class="title">${film.title}</div>`;
+        filmString += `<div class="yearVote"><span>${(film.release_date).slice(0, 4)}</span>`;
+        filmString += `<span class="vote">${film.vote_average}</span></div></div>`;
+
+        showFilm.innerHTML += filmString;
+    }
+}
